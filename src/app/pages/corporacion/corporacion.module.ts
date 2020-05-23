e8d3519006598a10a972c05f7388b231460eb3b8
edgon <edgon85@gmail.com>
2020-05-23 13:31:06 -0600

    agregado variables, creado modulos y fonts y colors

---
+++ src/app/pages/corporacion/corporacion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CorporacionRoutingModule } from './corporacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CorporacionRoutingModule
+  ]
+})
+export class CorporacionModule { }
---
+++ src/app/pages/cotizacion/cotizacion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CotizacionRoutingModule } from './cotizacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CotizacionRoutingModule
+  ]
+})
+export class CotizacionModule { }
