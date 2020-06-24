b95fbcafead7d307f4911501426533d7a9bdd1e7
edgon <edgon85@gmail.com>
2020-06-24 00:03:36 -0600

    corporacion implementada

--- src/app/pages/corporacion/corporacion.module.ts
+++ src/app/pages/corporacion/corporacion.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CorporacionRoutingModule } from './corporacion-routing.module';
 import { CorporacionComponent } from './components/corporacion/corporacion.component';
-
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [CorporacionComponent],
-  imports: [
-    CommonModule,
-    CorporacionRoutingModule
-  ]
+  imports: [CommonModule, CorporacionRoutingModule, SharedModule],
 })
-export class CorporacionModule { }
+export class CorporacionModule {}
