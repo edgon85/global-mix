3b2e4bfd7c198d7edc5fe8ed3ee4355ece7d9876
edgon <edgon85@gmail.com>
2020-06-25 11:06:10 -0600

    seccion proyectos implementada

--- src/app/pages/proyectos/proyectos-routing.module.ts
+++ src/app/pages/proyectos/proyectos-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ProyectDetailComponent } from './components/proyect-detail/proyect-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProyectosComponent,
+  },
+  {
+    path: ':id',
+    component: ProyectDetailComponent,
   },
 ];
 
--- src/app/pages/proyectos/proyectos.module.ts
+++ src/app/pages/proyectos/proyectos.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProyectosRoutingModule } from './proyectos-routing.module';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
-
+import { ProyectDetailComponent } from './components/proyect-detail/proyect-detail.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [ProyectosComponent],
-  imports: [
-    CommonModule,
-    ProyectosRoutingModule
-  ]
+  declarations: [ProyectosComponent, ProyectDetailComponent],
+  imports: [CommonModule, ProyectosRoutingModule, SharedModule],
 })
-export class ProyectosModule { }
+export class ProyectosModule {}
