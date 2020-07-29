54734e374f2757dac943375926cb89ba64c8741b
edgon <edgon85@gmail.com>
2020-07-29 10:22:07 -0600

    datos de fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAiSIcNaoK_ukgR7zEByjIJnJvfgn-PwbY',
+    authDomain: 'beastmain-df1c1.firebaseapp.com',
+    databaseURL: 'https://beastmain-df1c1.firebaseio.com',
+    projectId: 'beastmain-df1c1',
+    storageBucket: 'beastmain-df1c1.appspot.com',
+    messagingSenderId: '420209872242',
+    appId: '1:420209872242:web:4f35621db290ca500d61eb',
+  },
 };
