80b62aab6db9bd5708fa0b97c244d171b6a72fd6
Edwin Gonzalez <hgedwin@wedevgt.com>
2021-03-02 18:27:32 -0600

    reacomodando navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('menu', { static: true }) menu: ElementRef;
+  @ViewChild('iconBurger', { static: true }) iconBurger: ElementRef;
 
-  constructor() { }
+  mediaQuery = window.matchMedia('(max-width: 768px)');
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showHideMenu() {
+    if (this.mediaQuery.matches) {
+      if (this.menu.nativeElement.classList.contains('drop-menu')) {
+        this.menu.nativeElement.classList.remove('drop-menu');
+        this.menu.nativeElement.classList.add('menu');
+        this.iconBurger.nativeElement.classList.remove('icon-burger-checked');
+      } else {
+        this.menu.nativeElement.classList.remove('menu');
+        this.menu.nativeElement.classList.add('drop-menu');
+        this.iconBurger.nativeElement.classList.add('icon-burger-checked');
+      }
+    }
   }
-
 }
